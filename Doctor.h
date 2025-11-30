dc2ab73e5f7fba80d732d667f851d0ebbf491dda
Water-lily13 <kyrkasalexandra@gmail.com>
2025-11-30 16:41:43 -0500

    Add Doctor class definition with member functions

---
+++ Doctor.h
@@ -0,0 +1,54 @@
+//============================================================================
+// Names       : Julien Clavier and Alexandra Kyrkas
+// Assignment  : 4
+// File        : Doctor.h
+//============================================================================
+
+#ifndef DOCTOR_H_
+#define DOCTOR_H_
+
+#include <iostream>
+using namespace std;
+
+class Doctor {
+
+public:
+
+    // Constructors
+	Doctor();
+	Doctor(string, string, long int, string, int, long double, long double);
+
+	// Setters
+    void set_firstName(string a);
+    void set_lastName(string a);
+    void set_id(long int a);
+    void set_specialty(string a);
+    void set_yearsExperience(int a);
+    void set_baseSalary(long double a);
+    void set_performanceBonus(long double a);
+
+    // Getters
+    string get_firstName() const;
+    string get_lastName() const;
+    long int get_id() const;
+    string get_specialty() const;
+    int get_yearsExperience() const;
+    long double get_baseSalary() const;
+    long double get_performanceBonus() const;
+
+    // Member functions
+    long double Calculate_Compensation() const;
+    void Print_Doctor_Info();
+
+private:
+    // Attributes
+    string firstName;
+    string lastName;
+    long int id;
+    string specialty;
+    int yearsExperience;
+    long double baseSalary;
+    long double performanceBonus;
+};
+
+#endif /* DOCTOR_H_ */
